0d8ab49f71e13c7901ba2a1c40f9b590a6db9d5b
MarcusNotheis <marcus.notheis@sap.com>
2021-02-01 09:35:25 +0000

    Deploying to gh-pages from  @ 8fba1c3643111a61c5f99d7b7041399fc6662f8f 🚀

---
+++ static/js/72.10018a9b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkitchen-sink"]=this["webpackJsonpkitchen-sink"]||[]).push([[72],{180:function(t,e,n){"use strict";n.r(e);var i=n(10),a=n(11),c=n(214),s=n(215),r=n(223),u={tag:"ui5-side-navigation-sub-item",properties:{text:{type:String},selected:{type:Boolean},icon:{type:String}},events:{click:{}}},p=function(t){Object(c.a)(n,t);var e=Object(s.a)(n);function n(){return Object(i.a)(this,n),e.apply(this,arguments)}return Object(a.a)(n,null,[{key:"metadata",get:function(){return u}}]),n}(r.a);p.define(),e.default=p}}]);
+//# sourceMappingURL=72.10018a9b.chunk.js.map
---
+++ static/js/74.d2daac24.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkitchen-sink"]=this["webpackJsonpkitchen-sink"]||[]).push([[74],{165:function(t,e,n){"use strict";n.r(e);var i=n(10),a=n(11),c=n(214),r=n(215),s=n(223),u={tag:"ui5-cb-item",properties:{text:{type:String},additionalText:{type:String}}},p=function(t){Object(c.a)(n,t);var e=Object(r.a)(n);function n(){return Object(i.a)(this,n),e.apply(this,arguments)}return Object(a.a)(n,null,[{key:"metadata",get:function(){return u}}]),n}(s.a);p.define(),e.default=p}}]);
+//# sourceMappingURL=74.d2daac24.chunk.js.map
